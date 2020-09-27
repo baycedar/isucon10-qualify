b6e9df6d0bf94d0ead10968976b7d547dfd70ec0
isucon10-worker-1 <isucon@example.com>
2020-09-27 13:09:17 +0000

    modify deploy scripts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,7 +6,10 @@ NOW=`date +%Y%m%d-%H%M%S`
 
 GIT_BRANCH=$1
 
-ssh 192.168.33.11 ~/isuumo/bin/reload_db.sh "${GIT_BRANCH}"
-ssh 192.168.33.12 ~/isuumo/bin/reload_web_app.sh "${GIT_BRANCH}"
-ssh 192.168.33.12 ~/isuumo/bin/reload_app.sh "${GIT_BRANCH}"
+ssh 192.168.33.11 ~/isuumo/bin/pull_branch.sh "${GIT_BRANCH}"
+ssh 192.168.33.11 ~/isuumo/bin/reload_db.sh
+ssh 192.168.33.12 ~/isuumo/bin/pull_branch.sh "${GIT_BRANCH}"
+ssh 192.168.33.12 ~/isuumo/bin/reload_web_app.sh
+ssh 192.168.33.13 ~/isuumo/bin/pull_branch.sh "${GIT_BRANCH}"
+ssh 192.168.33.13 ~/isuumo/bin/reload_app.sh
 
--- bin/reload_web_app.sh
+++ bin/reload_web_app.sh
@@ -4,13 +4,6 @@ set -uex -o pipefail
 cd `dirname ${BASH_SOURCE:-${0}}`/../
 NOW=`date +%Y%m%d-%H%M%S`
 
-GIT_BRANCH=$1
-
-# prepare source codes
-git fetch origin
-git checkout "${GIT_BRANCH}"
-git pull origin "${GIT_BRANCH}"
-
 # reload environment variables
 cp -b ./conf/env.sh ~/env.sh
 

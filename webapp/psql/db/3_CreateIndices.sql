9b4749cf679b8df0b780a7ee5eeeaa801dbeb483
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2020-09-27 16:50:52 +0900

    add indices for WHERE

--- webapp/psql/db/3_CreateIndices.sql
+++ webapp/psql/db/3_CreateIndices.sql
@@ -3,3 +3,15 @@ CREATE INDEX ON estate USING btree (rent ASC, id ASC);
 CREATE INDEX ON chair USING btree (price ASC, id ASC);
 CREATE INDEX ON chair USING btree (popularity DESC, id ASC);
 CREATE INDEX ON estate USING btree (popularity DESC, id ASC);
+
+-- indices for WHERE or JOIN
+CREATE INDEX ON chair USING btree (stock);
+CREATE INDEX ON chair USING btree (height);
+CREATE INDEX ON chair USING btree (width);
+CREATE INDEX ON chair USING btree (depth);
+CREATE INDEX ON chair USING btree (kind);
+CREATE INDEX ON chair USING btree (color);
+CREATE INDEX ON estate USING btree (door_height);
+CREATE INDEX ON estate USING btree (door_width);
+CREATE INDEX ON estate USING btree (latitude);
+CREATE INDEX ON estate USING btree (longitude);

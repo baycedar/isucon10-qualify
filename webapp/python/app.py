c21ee292b33b4778e60ce06e1ecac40fb497b3ba
isucon10-worker-1 <isucon@example.com>
2020-09-28 23:07:05 +0000

    add workers and modify load balancing

--- webapp/python/app.py
+++ webapp/python/app.py
@@ -41,7 +41,7 @@ psql_connection_env = {
     "dbname": getenv("PGDATABASE", "isuumo"),
 }
 
-cnxpool = QueuePool(lambda: psycopg2.connect(**psql_connection_env, cursor_factory=RealDictCursor), pool_size=15)
+cnxpool = QueuePool(lambda: psycopg2.connect(**psql_connection_env, cursor_factory=RealDictCursor), pool_size=20)
 
 
 def select_all(query, *args):

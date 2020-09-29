fc2a750f9c583dfcbe6ae086c95db8d967feee32
isucon10-worker-1 <isucon@example.com>
2020-09-29 10:41:39 +0000

    modify db init script to call redis initialization

--- webapp/psql/db/init.sh
+++ webapp/psql/db/init.sh
@@ -12,3 +12,5 @@ export LANG="C.UTF-8"
 cd $CURRENT_DIR
 
 cat 0_Schema.sql 1_DummyEstateData.sql 2_DummyChairData.sql 3_CreateIndices.sql | psql -h $PGHOST -p $PGPORT -U $PGUSER -d $PGDATABASE
+ssh 192.168.33.12 /home/isucon/isuumo/webapp/redis/init.sh
+ssh 192.168.33.13 /home/isucon/isuumo/webapp/redis/init.sh

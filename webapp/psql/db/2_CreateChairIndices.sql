1c9c715ba660a800c354c3bcf217110b106ae627
isucon10-worker-2 <isucon@example.com>
2020-09-30 12:41:59 +0000

    fix db init scripts

--- webapp/psql/db/2_CreateChairIndices.sql
+++ webapp/psql/db/2_CreateChairIndices.sql
@@ -1,14 +1,13 @@
+-- indices for ORDER BY
+CREATE INDEX ON chair USING btree (price ASC, id ASC);
+CREATE INDEX ON chair USING btree (popularity DESC, id ASC);
+
 -- indices for WHERE or JOIN
 CREATE INDEX ON chair USING btree (height);
 CREATE INDEX ON chair USING btree (width);
 CREATE INDEX ON chair USING btree (depth);
 CREATE INDEX ON chair USING btree (kind);
 CREATE INDEX ON chair USING btree (color);
-CREATE INDEX ON estate USING btree (door_height);
-CREATE INDEX ON estate USING btree (door_width);
-CREATE INDEX ON estate USING btree (latitude);
-CREATE INDEX ON estate USING btree (longitude);
-CREATE INDEX ON estate USING gist (geom_coords);
 
 -- vacuum and analyze tables
 VACUUM ANALYZE chair;
--- webapp/psql/db/2_CreateEstateIndices.sql
+++ webapp/psql/db/2_CreateEstateIndices.sql
@@ -4,9 +4,14 @@ UPDATE estate SET
 
 -- indices for ORDER BY
 CREATE INDEX ON estate USING btree (rent ASC, id ASC);
-CREATE INDEX ON chair USING btree (price ASC, id ASC);
-CREATE INDEX ON chair USING btree (popularity DESC, id ASC);
 CREATE INDEX ON estate USING btree (popularity DESC, id ASC);
+
+-- indices for WHERE or JOIN
+CREATE INDEX ON estate USING btree (door_height);
+CREATE INDEX ON estate USING btree (door_width);
+CREATE INDEX ON estate USING btree (latitude);
+CREATE INDEX ON estate USING btree (longitude);
+CREATE INDEX ON estate USING gist (geom_coords);
 
 -- vacuum and analyze tables
 VACUUM ANALYZE estate;

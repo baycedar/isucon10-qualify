e49e61ce80444a2d2d759c7ecb652c6318bb0365
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2020-09-30 20:08:15 +0900

    fix init script

--- webapp/psql/db/init.sh
+++ webapp/psql/db/init.sh
@@ -12,5 +12,3 @@ export LANG="C.UTF-8"
 cd $CURRENT_DIR
 
 cat 0_Schema.sql 1_DummyEstateData.sql 2_DummyChairData.sql 3_CreateIndices.sql | psql -h $PGHOST -p $PGPORT -U $PGUSER -d $PGDATABASE
-ssh 192.168.33.12 /home/isucon/isuumo/webapp/redis/init.sh
-ssh 192.168.33.13 /home/isucon/isuumo/webapp/redis/init.sh

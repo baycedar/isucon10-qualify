831eb7c71dd6b18ccc4593b1327cad68660487a0
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2020-10-03 16:26:25 +0900

    modify script to limit file permission

--- bin/reload/environment.sh
+++ bin/reload/environment.sh
@@ -6,6 +6,7 @@ cd `dirname ${BASH_SOURCE:-${0}}`/../../
 # reload environment variables
 cp -b ./conf/env.sh ~/env.sh
 cp -b ./conf/pgpass ~/.pgpass
+chmod 600 ~/.pgpass
 
 # reload sysctl
 sudo cp -b ./conf/sysctl.conf /etc/sysctl.conf

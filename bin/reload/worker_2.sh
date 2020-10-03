c0e3dece1cce73507e9c1488bf994ea3365a23c9
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2020-10-03 23:58:23 +0900

    add app workers

--- bin/reload/worker_2.sh
+++ bin/reload/worker_2.sh
@@ -4,8 +4,8 @@ set -uex -o pipefail
 cd `dirname ${BASH_SOURCE:-${0}}`
 
 ./environment.sh
-./enable_app.sh
 ./disable_mysql.sh
 ./enable_nginx.sh
 ./disable_postgresql.sh
 ./disable_redis.sh
+./enable_app.sh
--- bin/reload/worker_3.sh
+++ bin/reload/worker_3.sh
@@ -4,8 +4,8 @@ set -uex -o pipefail
 cd `dirname ${BASH_SOURCE:-${0}}`
 
 ./environment.sh
-./disable_app.sh
 ./disable_mysql.sh
 ./disable_nginx.sh
 ./enable_postgresql.sh
 ./disable_redis.sh
+./enable_app.sh

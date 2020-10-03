648f3f8cdaeb33daa4bb61daebd750f82942a2d3
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2020-10-03 16:02:01 +0900

    modify how to load environment variables

--- bin/reload/environment.sh
+++ bin/reload/environment.sh
@@ -4,7 +4,7 @@ set -uex -o pipefail
 cd `dirname ${BASH_SOURCE:-${0}}`/../../
 
 # reload environment variables
-cp -b ./conf/env.sh ~/env.sh
+source ./conf/env.sh
 
 # reload sysctl
 sudo cp -b ./conf/sysctl.conf /etc/sysctl.conf

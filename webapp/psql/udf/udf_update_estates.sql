5e4077edff0cc7428befa9e13dde457f7986bd23
isucon10-worker-2 <isucon@example.com>
2020-10-04 05:11:05 +0000

    fix trigger

--- webapp/psql/udf/udf_update_estates.sql
+++ webapp/psql/udf/udf_update_estates.sql
@@ -1,11 +1,13 @@
 DROP FUNCTION IF EXISTS update_estates;
 CREATE OR REPLACE FUNCTION update_estates ()
-RETURNS VOID
-STABLE AS $$
+RETURNS TRIGGER
+VOLATILE AS $$
 DECLARE
 BEGIN
   UPDATE estate SET
-    geom_coords = ST_MakePoint(longitude, latitude);
-  RETURN;
+    geom_coords = ST_MakePoint(longitude, latitude)
+  WHERE
+    geom_coords = ST_MakePoint(0, 0);
+  RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;

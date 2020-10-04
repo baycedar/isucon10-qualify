fc5215b6d8209e5dd8cec36d476377589fdc5335
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2020-10-04 19:48:45 +0900

    modify config for 2-app:1-db architecture

--- webapp/psql/init.sh
+++ webapp/psql/init.sh
@@ -3,8 +3,7 @@ set -xe
 set -o pipefail
 
 CURRENT_DIR=$(cd $(dirname $0);pwd)
-export PG_ESTATE_HOST=${PG_ESTATE_HOST:-127.0.0.1}
-export PG_CHAIR_HOST=${PG_CHAIR_HOST:-127.0.0.1}
+export PGHOST=${PGHOST:-127.0.0.1}
 export PGPORT=${PGPORT:-5432}
 export PGUSER=${PGUSER:-isucon}
 export PGDATABASE=${PGDATABASE:-isuumo}
@@ -12,14 +11,13 @@ export PGPASSWORD=${PGPASSWORD:-isucon}
 export LANG="C.UTF-8"
 cd $CURRENT_DIR
 
-psql -h $PG_ESTATE_HOST -p $PGPORT -U $PGUSER -d $PGDATABASE \
+psql -h $PGHOST -p $PGPORT -U $PGUSER -d $PGDATABASE \
   -f ./db/0_EstateSchema.sql \
   -f ./udf/udf_update_estates.sql \
   -f ./udf/trigger_estate_insert.sql \
   -f ./db/1_DummyEstateData.sql \
   -f ./db/2_CreateEstateIndices.sql \
-  -f ./db/3_PostProcessForEstate.sql
-psql -h $PG_CHAIR_HOST -p $PGPORT -U $PGUSER -d $PGDATABASE \
+  -f ./db/3_PostProcessForEstate.sql \
   -f ./db/0_ChairSchema.sql \
   -f ./db/1_DummyChairData.sql \
   -f ./db/2_CreateChairIndices.sql \

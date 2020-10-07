c734a4884f49271ffd5a78dbb4a9597abd2102b4
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2020-10-08 00:04:18 +0900

    remove trigger from estate

--- webapp/psql/init.sh
+++ webapp/psql/init.sh
@@ -14,8 +14,6 @@ cd $CURRENT_DIR
 
 psql -h $PG_ESTATE_HOST -p $PGPORT -U $PGUSER -d $PGDATABASE \
   -f ./db/0_EstateSchema.sql \
-  -f ./udf/udf_update_estates.sql \
-  -f ./udf/trigger_estate_insert.sql \
   -f ./db/1_DummyEstateData.sql \
   -f ./db/2_CreateEstateIndices.sql \
   -f ./db/3_PostProcessForEstate.sql

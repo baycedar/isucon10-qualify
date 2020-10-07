c0c411aa76b12244a8e2e2cb58e0e85054acf29c
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2020-10-07 21:18:23 +0900

    modify architecture to app-2:db-1

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -300,8 +300,8 @@ func main() {
 	if err != nil {
 		e.Logger.Fatalf("DB connection failed : %v", err)
 	}
-	db.SetMaxOpenConns(90)
-	db.SetMaxIdleConns(90)
+	db.SetMaxOpenConns(40)
+	db.SetMaxIdleConns(40)
 	db.SetConnMaxLifetime(60 * time.Second)
 	defer db.Close()
 

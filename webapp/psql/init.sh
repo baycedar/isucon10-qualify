895b37e7ab0395e7d4bfcbbc058008603265bf36
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2020-10-07 22:50:54 +0900

    modify connection settings for DBs

--- webapp/psql/init.sh
+++ webapp/psql/init.sh
@@ -3,7 +3,8 @@ set -xe
 set -o pipefail
 
 CURRENT_DIR=$(cd $(dirname $0);pwd)
-export PGHOST=${PGHOST:-127.0.0.1}
+export PG_ESTATE_HOST=${PG_ESTATE_HOST:-127.0.0.1}
+export PG_ESTATE_HOST=${PG_CHAIR_HOST:-127.0.0.1}
 export PGPORT=${PGPORT:-5432}
 export PGUSER=${PGUSER:-isucon}
 export PGDATABASE=${PGDATABASE:-isuumo}
@@ -11,13 +12,14 @@ export PGPASSWORD=${PGPASSWORD:-isucon}
 export LANG="C.UTF-8"
 cd $CURRENT_DIR
 
-psql -h $PGHOST -p $PGPORT -U $PGUSER -d $PGDATABASE \
+psql -h $PG_ESTATE_HOST -p $PGPORT -U $PGUSER -d $PGDATABASE \
   -f ./db/0_EstateSchema.sql \
   -f ./udf/udf_update_estates.sql \
   -f ./udf/trigger_estate_insert.sql \
   -f ./db/1_DummyEstateData.sql \
   -f ./db/2_CreateEstateIndices.sql \
-  -f ./db/3_PostProcessForEstate.sql \
+  -f ./db/3_PostProcessForEstate.sql
+psql -h $PG_CHAIR_HOST -p $PGPORT -U $PGUSER -d $PGDATABASE \
   -f ./db/0_ChairSchema.sql \
   -f ./db/1_DummyChairData.sql \
   -f ./db/2_CreateChairIndices.sql \

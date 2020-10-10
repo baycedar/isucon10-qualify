d51147f2dbf7061c51a2fe33b03bef499f2b2ab5
isucon10-worker-1 <isucon@example.com>
2020-10-10 02:33:58 +0000

    fix deploy script

--- bin/reload/enable_postgresql.sh
+++ bin/reload/enable_postgresql.sh
@@ -14,3 +14,4 @@ sudo chown postgres:adm /var/log/postgresql/postgresql-12-main.log
 sudo cp -b ./conf/postgresql.conf /etc/postgresql/12/main/postgresql.conf
 sudo cp -b ./conf/pg_hba.conf /etc/postgresql/12/main/pg_hba.conf
 sudo systemctl restart postgresql.service
+sudo systemctl enable postgresql.service

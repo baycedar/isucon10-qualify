2a6a73669a7816db4e3230d94a377a73bd7d279d
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2020-10-10 14:50:27 +0900

    add progress messages to deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,11 +6,12 @@ WORKSPACE=$(cd $(dirname ${BASH_SOURCE:-${0}})/../; pwd)
 usage() {
   cat 1>&2 << EOS
 Usage:
-  ${BASH_SOURCE:-${0}} <branch_name>
+  ${BASH_SOURCE:-${0}} <branch_name> 2> <error_log>
 Description:
   Pull and deploy source codes to workers.
 Arguments:
   <branch_name>: A branch name to deploy source codes.
+  <error_log>: A log file to forward stderr messages.
 EOS
   exit 1
 }
@@ -34,22 +35,30 @@ git checkout "${GIT_BRANCH}"
 git merge --no-progress "origin/${GIT_BRANCH}"
 
 # load environment variables
+echo "load environment variables..."
 source ${WORKSPACE}/conf/env.sh
 
 # initialization
+echo "initialize workers..."
 for WORKER in ${WORKERS}; do
-  # fetch the specified branch on all workers
+  echo "${WORKER}:"
+  echo "  fetch remote sources..."
   ssh ${WORKER} ${WORKSPACE}/bin/reload/branch.sh "${GIT_BRANCH}"
-  # stop/disable all functions
+  echo "  disable all services..."
   ssh ${WORKER} ${WORKSPACE}/bin/reload/disable_all.sh
-  # update server environment
+  echo "  update server environment..."
   ssh ${WORKER} ${WORKSPACE}/bin/reload/environment.sh
+  echo "${WORKER} done."
 done
 
 # start/enable service
+echo "start a database server on ${PG_ESTATE_HOST}"
 ssh ${PG_ESTATE_HOST} ${WORKSPACE}/bin/reload/enable_postgresql.sh
+echo "start a database server on ${PG_CHAIR_HOST}"
 ssh ${PG_CHAIR_HOST} ${WORKSPACE}/bin/reload/enable_postgresql.sh
 for APP_HOST in ${APP_HOSTS}; do
+  echo "start an app-server on ${APP_HOST}"
   ssh ${APP_HOST} ${WORKSPACE}/bin/reload/enable_app.sh
 done
+echo "start a web-server on ${WEB_HOST}"
 ssh ${WEB_HOST} ${WORKSPACE}/bin/reload/enable_nginx.sh

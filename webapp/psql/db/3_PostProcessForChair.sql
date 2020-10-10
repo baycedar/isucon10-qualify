c7fa5f1025a8196c276d702500d7dc172866aeb6
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2020-10-10 09:36:12 +0900

    add pg_warm extension for pre-fetch

--- webapp/psql/db/3_PostProcessForChair.sql
+++ webapp/psql/db/3_PostProcessForChair.sql
@@ -3,3 +3,6 @@ VACUUM ANALYZE chair;
 
 -- reset statistics
 SELECT pg_stat_statements_reset();
+
+-- fetch initial data in memory
+SELECT pg_prewarm('chair', 'prefetch');
--- webapp/psql/db/3_PostProcessForEstate.sql
+++ webapp/psql/db/3_PostProcessForEstate.sql
@@ -3,3 +3,6 @@ VACUUM ANALYZE estate;
 
 -- reset statistics
 SELECT pg_stat_statements_reset();
+
+-- fetch initial data in memory
+SELECT pg_prewarm('estate', 'prefetch');

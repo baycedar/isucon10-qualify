2318ddf4b4eb2dbe89f2c495f63f2cda8723bb8f
isucon10-worker-2 <isucon@example.com>
2020-10-11 08:01:53 +0000

    disable logs
    This reverts commit 9bec42a5ce67d18deec3e1bd1189f4346d9137b6.

--- webapp/psql/db/3_PostProcessForChair.sql
+++ webapp/psql/db/3_PostProcessForChair.sql
@@ -2,7 +2,7 @@
 VACUUM ANALYZE chair;
 
 -- reset statistics
-SELECT pg_stat_statements_reset();
+--SELECT pg_stat_statements_reset();
 
 -- fetch initial data in memory
 SELECT pg_prewarm('chair', 'prefetch');
